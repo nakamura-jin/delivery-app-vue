e566e0271b1825a257b37b6574431dae48cb7d7b
中村　迅 <shu.kyu0224@icloud.com>
2021-12-07 17:43:05 +0900

    axios beseURL変更

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,3 +1,4 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000'
+// axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'http://test-demo-site.com'
