b1eb9ff0cf846a81ef65ce6f0c94b70c0c3af6fe
中村　迅 <shu.kyu0224@icloud.com>
2021-12-16 16:16:26 +0900

    baseURL編集

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,4 +2,4 @@ import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:8000'
 // axios.defaults.baseURL = 'https://test-demo-site.herokuapp.com'
-axios.defaults.baseURL = '54.249.188.3'
+axios.defaults.baseURL = 'http://54.249.188.3'
